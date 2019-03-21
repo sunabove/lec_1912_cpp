eb1d1505bc05e35c584b3224d9c45895720addf9
sunabove <sunabove@users.noreply.github.com>
2019-03-22 00:17:41 +0900

    **

--- ConsoleApplication1/ConsoleApplication1/ArgumentPrint.cpp
+++ ConsoleApplication1/ConsoleApplication1/ArgumentPrint.cpp
@@ -5,11 +5,11 @@
 
 using namespace std;
 
-int main(int argc, char ** argv) {
+int main(int argc, char ** argv){
 	
 	cout << "argc = " << argc << endl; 
 	
-	for (int i = 0 ; i < argc ; i++) {
+	for(int i = 0 ; i < argc ; i++){
 		cout << "argv[" << i << "] = " << argv[i] << endl;		
 	}
 
