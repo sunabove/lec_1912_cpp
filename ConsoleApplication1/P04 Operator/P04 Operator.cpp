624585bd7c6e8a558558571ec55eab818b0fac11
sunabove <sunabove@users.noreply.github.com>
2019-03-23 02:13:08 +0900

    **

---
+++ ConsoleApplication1/P04 Operator/P04 Operator.cpp
@@ -0,0 +1,12 @@
+﻿// P04 Operator.cpp
+#include "pch.h"
+#include <iostream>
+using namespace std;
+
+int main(){
+	int x = 3, y = 6, z = 10;
+	cout << "3 * 6 is " << x * y << endl
+		<< "6 / 3 is " << y / x << endl
+		<< "10 % 3 is " << z % x << endl
+		<< "10 / 3 is " << (float)z / x << endl;
+} 
