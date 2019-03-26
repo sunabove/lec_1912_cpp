c76e023ca49c6815d09799d9120879bb622443ae
sunabove <sunabove@users.noreply.github.com>
2019-03-27 02:36:43 +0900

    ((

--- ConsoleApplication1/P04 Operator/P04 Operator.cpp
+++ ConsoleApplication1/P04 Operator/P04 Operator.cpp
@@ -20,5 +20,16 @@ int main(){
 			<< "10 / 3 is " << (float)z / x << endl;
 	}
 
+	int remainder = 3 % 2;
+
+	bool isEven = ( remainder == 0 ) ; 
+
+	remainder = 4 % 2;
+
+	bool isOdd = ( remainder == 1 ); 
+
+	cout << isEven << endl;
+	cout << isOdd << endl; 
+
 	return 0;
 } 
---
+++ ConsoleApplication1/P05 Control/P05 Control.cpp
@@ -0,0 +1,47 @@
+﻿// P05 Control.cpp : 이 파일에는 'main' 함수가 포함됩니다.
+
+#include "pch.h"
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	// The counter variable can be declared in the init-expression.
+	for (int i = 0; i < 2; i++) {
+		cout << i;
+	}
+	// Output: 01
+	// The counter variable can be declared outside the for loop.
+	int i;
+	for (i = 0; i < 2; i++) {
+		cout << i;
+	}
+	// Output: 01
+	int j;
+	for (i = 5, j = 10; i + j < 20; i++, j++) {
+		cout << "i + j = " << (i + j) << '\n';
+	}
+
+	// Output: 10 9 8 7 6 5 4 3 2 1
+	for (int i = 10; i < 20; i = i + 2) {
+		cout << i << ' ';
+	}
+
+	for (int i = 10; i > 0; i--) {
+		cout << i << ' ';
+	}
+	// Output: 10 9 8 7 6 5 4 3 2 1
+	for (int i = 10; i < 20; i = i + 2) {
+		cout << i << ' ';
+	}
+	// Output: 10 12 14 16 18
+
+	for (i = 0; i < 5; cout << i << '\n', i++) {
+		;
+	}
+
+	i = 0;
+	while (i < 2) {
+		cout << i++;
+	}
+}
