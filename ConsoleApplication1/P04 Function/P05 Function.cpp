c3ebe491e449b76022a90117bd39df24f10d4866
sunabove <sunabove@users.noreply.github.com>
2019-03-26 23:43:54 +0900

    **

---
+++ ConsoleApplication1/P04 Function/P05 Function.cpp
@@ -0,0 +1,20 @@
+﻿// P04 Function.cpp : 이 파일에는 'main' 함수가 포함됩니다.
+#include "pch.h"
+#include <iostream>
+
+using namespace std; 
+
+int sum(int a, int b); // 함수 선언
+
+int main() { // 함수 정의
+	int i = sum(10, 32); // 함수 호출
+	int j = sum(i, 66);  // 함수 호출
+
+	cout << "The value of j is " << j << endl; // 108
+
+	return 0; 
+} 
+
+int sum(int a, int b) { // 함수 정의
+	return a + b;
+}
--- ConsoleApplication1/P04 Operator/P04 Operator.cpp
+++ ConsoleApplication1/P04 Operator/P04 Operator.cpp
@@ -4,9 +4,21 @@
 using namespace std;
 
 int main(){
-	int x = 3, y = 6, z = 10;
-	cout << "3 * 6 is " << x * y << endl
-		<< "6 / 3 is " << y / x << endl
-		<< "10 % 3 is " << z % x << endl
-		<< "10 / 3 is " << (float)z / x << endl;
+	{
+		int x = 3, y = 6, z = 10;
+		cout << "3 * 6 is " << x * y << endl
+			<< "6 / 3 is " << y / x << endl
+			<< "10 % 3 is " << z % x << endl
+			<< "10 / 3 is " << (float)z / x << endl;
+	}
+
+	{
+		double x = 3, y = 6, z = 10;
+		cout << "3 * 6 is " << x * y << endl
+			<< "6 / 3 is " << y / x << endl
+			// << "10 % 3 is " << z % x << endl
+			<< "10 / 3 is " << (float)z / x << endl;
+	}
+
+	return 0;
 } 
