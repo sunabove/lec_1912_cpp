f7d7b6d6f8130aba16a6952b8f86bf53dc6fea88
sunabove <sunabove@users.noreply.github.com>
2019-04-09 02:54:29 +0900

    **

--- ConsoleApplication1/P05 Control/P05 Control.cpp
+++ ConsoleApplication1/P05 Control/P05 Control.cpp
@@ -5,43 +5,42 @@
 
 using namespace std;
 
-int main() {
-	// The counter variable can be declared in the init-expression.
-	for (int i = 0; i < 2; i++) {
+int main(){
+	
+	for (int i = 0; i < 2; i++){
 		cout << i;
 	}
-	// Output: 01
-	// The counter variable can be declared outside the for loop.
+	
 	int i;
-	for (i = 0; i < 2; i++) {
+	for (i = 0; i < 2; i++){
 		cout << i;
 	}
-	// Output: 01
+	
 	int j;
-	for (i = 5, j = 10; i + j < 20; i++, j++) {
+	for (i = 5, j = 10; i + j < 20; i++, j++){
 		cout << "i + j = " << (i + j) << '\n';
 	}
 
-	// Output: 10 9 8 7 6 5 4 3 2 1
-	for (int i = 10; i < 20; i = i + 2) {
+	for (int i = 10; i < 20; i = i + 2){
 		cout << i << ' ';
 	}
 
-	for (int i = 10; i > 0; i--) {
+	for (int i = 10; i > 0; i--){
 		cout << i << ' ';
 	}
-	// Output: 10 9 8 7 6 5 4 3 2 1
-	for (int i = 10; i < 20; i = i + 2) {
+	
+	for (int i = 10; i < 20; i = i + 2){
 		cout << i << ' ';
 	}
-	// Output: 10 12 14 16 18
-
-	for (i = 0; i < 5; cout << i << '\n', i++) {
+	
+	for (i = 0; i < 5; cout << i << '\n', i++){
 		;
 	}
 
 	i = 0;
-	while (i < 2) {
+	while (i < 2){
 		cout << i++;
 	}
+	
+	return 0;
 }
