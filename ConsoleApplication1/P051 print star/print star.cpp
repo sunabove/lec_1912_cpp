c2a94df5bc189a7512e5205ac21b7893571f5364
sunabove <sunabove@users.noreply.github.com>
2019-04-14 11:32:04 +0900

    **

--- ConsoleApplication1/P051 print star/print star.cpp
+++ ConsoleApplication1/P051 print star/print star.cpp
@@ -1,9 +1,16 @@
 // 별표(*)를 출력하는 프로그램 입니다.
 
 #include <iostream>
+using namespace std; 
 
 int main() {
-    std::cout << "Hello World!\n"; 
+
+	for (int i = 1; i <= 8; i++) {
+		for (int k = 1; k < i; k++) {
+			cout << "* ";
+		}
+		cout << endl;
+	}
 
 	return 0;
 }
