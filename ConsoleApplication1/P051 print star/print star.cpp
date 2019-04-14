154fa093d294e35043d11e5d7ab4e113b4124ad4
sunabove <sunabove@users.noreply.github.com>
2019-04-14 11:43:18 +0900

    **

--- ConsoleApplication1/P051 print star/print star.cpp
+++ ConsoleApplication1/P051 print star/print star.cpp
@@ -7,7 +7,7 @@ int main() {
 
 	for (int i = 1; i <= 8; i++) {
 		for (int k = 1; k < i; k++) {
-			cout << "* ";
+			cout << "*";
 		}
 		cout << endl;
 	}
--- ConsoleApplication1/print_multiple_number/print_multiple_number.cpp
+++ ConsoleApplication1/print_multiple_number/print_multiple_number.cpp
@@ -1,9 +1,14 @@
 // 배수를 출력하는 프로그램입니다.
 
 #include <iostream>
+using namespace std; 
 
-int main()
-{
-    std::cout << "Hello World!\n"; 
+int main() {
+	for (int i = 1; i < 100; i++) {
+		if (i % 3 == 0) {
+			cout << i << ", ";
+		}
+	}
+	cout << endl; 
 }
 
