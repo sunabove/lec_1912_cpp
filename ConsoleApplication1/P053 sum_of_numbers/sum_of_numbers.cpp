fae0ed94b6d955461cc8ee85a11f30f407cff90d
sunabove <sunabove@users.noreply.github.com>
2019-04-14 11:56:40 +0900

    **

--- ConsoleApplication1/P053 sum_of_numbers/sum_of_numbers.cpp
+++ ConsoleApplication1/P053 sum_of_numbers/sum_of_numbers.cpp
@@ -1,9 +1,16 @@
 // 수열의 합의 구하는 프로그램입니다.
 
 #include <iostream>
+using namespace std; 
 
-int main()
-{
-    std::cout << "Hello World!\n"; 
+int main(){
+	double sum = 0;
+	for (int d = 1; d <= 100; d++) {
+		sum = sum + (1.0/d); 
+	}
+
+	cout << "sum = " << sum << endl ; 
+
+	return 0; 
 }
 
